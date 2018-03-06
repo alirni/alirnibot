a49a6a3a42e599ad1adff5549ed2780a04010304
alirni <aliramezani19.1@gmail.com>
2018-03-06 23:55:52 +0330

    feat(index): add bio and portfolio

--- index.js
+++ index.js
@@ -22,7 +22,29 @@
     console.log(success('run start command,'), message.chat);
 
     try {
-      const text = `... سلام ${message.chat.first_name} عزیز خوش آمدید.\n..: www.ramezany.com :..\n\n📌 نمونه کار ها: /portfolio\n\n▪️ تلگرام: @AliRniE\n▪️ ایمیل: aliramezani19.1@gmail.com\n▪️ گیتهاب: https://github.com/alirni\n▪️ اینستاگرام: https://www.instagram.com/alirni/`;
+      const
+        bio = 'من علی رمضانی هستم ✋️\n🎓 کارشناسی برنامه نویسی تحت وب دارم و در کنار کار برنامه نویسی به گرافیک هم علاقه دارم.\nدر حوضه‌ی Progressive Web Applications فعالیت دارم و جزء مشارکت کنندگان کامپوننت های Polymer هستم.\nدر حال حاظر در شرکت پیشتازان توسعه به عنوان توسعه دهنده وب مشغولم.',
+        menu = '📌 نمونه کار ها: /portfolio',
+        contact = '▪️ تلگرام: @AliRniE\n▪️ ایمیل: aliramezani19.1@gmail.com\n▪️ گیتهاب: https://github.com/alirni',
+        text = `... سلام ${message.chat.first_name} عزیز خوش آمدید.\n\n${bio}\n\n.: www.ramezany.com :.\n${menu}\n\n${contact}`
+      ;
+      await bot.sendMessage({
+        chat_id: message.chat.id,
+        text: text,
+      });
+
+      console.log(success('message sent'));
+    }
+    catch (error) {
+      console.log(error('message error:'), error);
+    }
+  });
+
+  bot.onMessage(new RegExp('^/portfolio[ ]*$'), async (message) => {
+    console.log(success('run portfolio command,'), message.chat);
+
+    try {
+      const text = '▪️ طراحی سایت: /web\n▪️ طراحی لوگو: /logo\n▪️ کارت ویزیت: /card';
       await bot.sendMessage({
         chat_id: message.chat.id,
         text: text,
