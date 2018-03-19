1561018b43f82231344e9be64a036d7bb788bc3f
alirni <aliramezani19.1@gmail.com>
2018-03-19 10:33:23 +0330

    fix(index): remove lib

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ chalk = require('chalk'),
 error = chalk.bold.bgRed,
 success = chalk.blue.bold,
 
-lib = require('./lib'),
 bot = require('./bot'),
-
-text = lib.readJsonFile('./text.json'),
-menu = lib.readJsonFile('./menu.json'),
+text = require('./text.json'),
 
 registerAllCommand = () => {
-  bot.onMessage(new RegExp('^/start[ ]*$'), startCommand);
-  bot.onMessage(new RegExp('^/portfolio[ ]*$'), portfolioCommand);
+  bot.onMessage(new RegExp('^/start$'), startCommand);
+  bot.onMessage(new RegExp('^/portfolio$'), portfolioCommand);
 
   console.log(success('registered all command'));
 },
@@ -22,7 +19,7 @@ startCommand = async (message) => {
   console.log(success('run start command,'), message.text);
 
   try {
-    const botMessage = `🤖 سلام ${message.chat.first_name} عزیز خوش آمدید.\n\n${text.bio}\n\n🌐 www.ramezany.com 🌐\n${text.menu}\n\n${text.contact}`;
+    const botMessage = `🤖 سلام ${message.chat.first_name} عزیز خوش آمدید.\n\n${text.bio}\n\n🌐 www.ramezany.com 🌐\n\n${text.menu.portfolio}\n\n${text.contact}`;
 
     await bot.sendMessage({
       chat_id: message.chat.id,
@@ -39,7 +36,7 @@ startCommand = async (message) => {
 portfolioCommand = async (message) => {
   console.log(success('run portfolio command,'), message.text);
   try {
-    const botMessage = `${menu.web}\n${menu.logo}\n${menu.card}\n`;
+    const botMessage = `${text.menu.web}\n${text.menu.logo}\n${text.menu.card}\n`;
     await bot.sendMessage({
       chat_id: message.chat.id,
       text: botMessage,
